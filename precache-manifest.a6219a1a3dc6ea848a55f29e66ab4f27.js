64a6846a13893fba48cf0345384eb4685e220746
iamangelu <67723309+iamangelu@users.noreply.github.com>
2020-07-26 16:54:12 +0800

    Updates

---
+++ precache-manifest.a6219a1a3dc6ea848a55f29e66ab4f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0d3a66da2a60d70e63e6b24583e5e5",
+    "url": "/dice-roll/index.html"
+  },
+  {
+    "revision": "402c5675a52b16aa3757",
+    "url": "/dice-roll/static/css/main.10437930.chunk.css"
+  },
+  {
+    "revision": "2077b8e21efba145bee7",
+    "url": "/dice-roll/static/js/2.fc8578a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-roll/static/js/2.fc8578a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "402c5675a52b16aa3757",
+    "url": "/dice-roll/static/js/main.5b2c6357.chunk.js"
+  },
+  {
+    "revision": "27f4aa544f09f0701617",
+    "url": "/dice-roll/static/js/runtime-main.61a9402f.js"
+  }
+]);
